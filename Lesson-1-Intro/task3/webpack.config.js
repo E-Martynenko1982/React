5b25c2ff0921e1e967509071a01f3d7f05249b16
E-Martynenko1982 <motor8103@gmail.com>
2024-08-09 12:47:53 +0300

    update

--- Lesson-1-Intro/task3/webpack.config.js
+++ Lesson-1-Intro/task3/webpack.config.js
@@ -1,6 +1,11 @@
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-
+const path = require('path');
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'review_build'),
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
@@ -17,5 +22,9 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: "./src/index.html",
     })
-  ]
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'review_build'),  // Папка, откуда сервер будет обслуживать файлы
+    open: true,  // Автоматически открывать браузер после запуска
+  },
 }
