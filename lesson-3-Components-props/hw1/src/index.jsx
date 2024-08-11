4b2d3aa7d689f6d22522e55b8bf95e12e8d52c44
E-Martynenko1982 <motor8103@gmail.com>
2024-08-11 21:27:16 +0300

    update

---
+++ lesson-3-Components-props/hw1/src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from './Greeting';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+const calculateAge = (birthDate) => {
+  const today = new Date();
+  const birthYear = birthDate.getFullYear();
+  const birthMonth = birthDate.getMonth();
+  const birthDay = birthDate.getDate();
+
+  let age = today.getFullYear() - birthYear;
+
+  // Перевіряємо, якщо цьогорічний день народження вже минув чи ще ні
+  const isBirthdayPassed = today.getMonth() > birthMonth || (today.getMonth() === birthMonth && today.getDate() >= birthDay);
+
+  // Якщо день народження ще не був цього року, зменшуємо вік на 1
+  if (!isBirthdayPassed) {
+    age -= 1;
+  }
+
+  return age;
+};
+
+const element = (
+  <Greeting
+    firstName='John'
+    lastName="Doe"
+    birthDate={calculateAge(new Date(2007, 7, 11))}
+  />
+)
+
+
+ReactDOM.render(element, rootElement);
