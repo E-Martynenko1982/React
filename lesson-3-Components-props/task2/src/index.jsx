0bd30d194f5f26da40194593b750b99dfeba73ab
E-Martynenko1982 <motor8103@gmail.com>
2024-08-11 16:53:02 +0300

    update

---
+++ lesson-3-Components-props/task2/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
+import './index.scss';
+
+
+const rootElement = document.querySelector('#root');
+const element = (
+  <Search name="Tom" />
+)
+
+
+ReactDOM.render(element, rootElement);
