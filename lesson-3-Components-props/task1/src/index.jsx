4c3b954d9df9d62fabbd729cae9848f1c682885f
E-Martynenko1982 <motor8103@gmail.com>
2024-08-11 15:05:09 +0300

    update

---
+++ lesson-3-Components-props/task1/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Search } from './search';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+  <Search name="Tom" />
+)
+
+
+
+
+
+ReactDOM.render(element, rootElement);
