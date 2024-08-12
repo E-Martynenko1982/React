c7c65e3f0c808a9ecadeaedb9d5105f948d1a275
E-Martynenko1982 <motor8103@gmail.com>
2024-08-12 10:35:09 +0300

    update

--- lesson-3-Components-props/hw1/src/Greeting.jsx
+++ lesson-3-Components-props/hw1/src/Greeting.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 const Greeting = ({ userData }) => {
+  const calculateAge = (birthDate) => {
+    const today = new Date();
+    const birthYear = birthDate.getFullYear();
+    const birthMonth = birthDate.getMonth();
+    const birthDay = birthDate.getDate();
 
+    let age = today.getFullYear() - birthYear;
+
+    // Перевіряємо, якщо цьогорічний день народження вже минув чи ще ні
+    const isBirthdayPassed = today.getMonth() > birthMonth || (today.getMonth() === birthMonth && today.getDate() >= birthDay);
+
+    // Якщо день народження ще не був цього року, зменшуємо вік на 1
+    if (!isBirthdayPassed) {
+      age -= 1;
+    }
+
+    return age;
+  };
 
   return (
     <div className="greeting">
-      {`My name is ${userData.firstName} ${userData.lastName}. I am ${userData.birthDate} years old`}
+      {`My name is ${userData.firstName} ${userData.lastName}. 
+      I am ${calculateAge(userData.birthDate)} years old`}
     </div>
   )
 
--- lesson-3-Components-props/hw1/src/index.jsx
+++ lesson-3-Components-props/hw1/src/index.jsx
@@ -4,28 +4,11 @@ import Greeting from './Greeting';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
-const calculateAge = (birthDate) => {
-  const today = new Date();
-  const birthYear = birthDate.getFullYear();
-  const birthMonth = birthDate.getMonth();
-  const birthDay = birthDate.getDate();
 
-  let age = today.getFullYear() - birthYear;
-
-  // Перевіряємо, якщо цьогорічний день народження вже минув чи ще ні
-  const isBirthdayPassed = today.getMonth() > birthMonth || (today.getMonth() === birthMonth && today.getDate() >= birthDay);
-
-  // Якщо день народження ще не був цього року, зменшуємо вік на 1
-  if (!isBirthdayPassed) {
-    age -= 1;
-  }
-
-  return age;
-};
 const userData = {
   firstName: 'John',
   lastName: "Doe",
-  birthDate: calculateAge(new Date(2007, 7, 11))
+  birthDate: new Date(2007, 7, 11)
 }
 
 const element = (
