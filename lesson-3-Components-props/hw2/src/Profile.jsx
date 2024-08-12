942c7c41db24c76bb0fab22ec617db9610f3e6e6
E-Martynenko1982 <motor8103@gmail.com>
2024-08-12 11:35:47 +0300

    update

--- lesson-3-Components-props/hw2/src/Profile.jsx
+++ lesson-3-Components-props/hw2/src/Profile.jsx
@@ -4,15 +4,15 @@ import './profile.scss';
 
 const formattedDate = date => moment(date).format('DD MMM YY');
 
-const Profile = (props) => {
-  console.log(props)
+const Profile = (userData) => {
+
 
   return (
     <>
       <ul className="profile">
-        <li className="profile__name">{`${props.name} ${props.lastName}`}</li>
+        <li className="profile__name">{`${userData.name} ${userData.lastName}`}</li>
         <li className="profile__birth">
-          Was born {formattedDate(props.birthDate)} in {props.birthPlace}
+          Was born {formattedDate(userData.birthDate)} in {userData.birthPlace}
         </li>
       </ul>
     </>
