956c7d7b530045d51af366f725008203f7fc6e9d
E-Martynenko1982 <motor8103@gmail.com>
2024-08-12 10:39:40 +0300

    update

--- lesson-3-Components-props/hw1/src/Greeting.jsx
+++ lesson-3-Components-props/hw1/src/Greeting.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 
+const calculateAge = (birthDate) => {
+  const today = new Date();
+  const birthYear = birthDate.getFullYear();
+  const birthMonth = birthDate.getMonth();
+  const birthDay = birthDate.getDate();
+
+  let age = today.getFullYear() - birthYear;
+
+  // Перевіряємо, якщо цьогорічний день народження вже минув чи ще ні
+  const isBirthdayPassed = today.getMonth() > birthMonth || (today.getMonth() === birthMonth && today.getDate() >= birthDay);
+
+  // Якщо день народження ще не був цього року, зменшуємо вік на 1
+  if (!isBirthdayPassed) {
+    age -= 1;
+  }
+
+  return age;
+};
 const Greeting = ({ userData }) => {
-  const calculateAge = (birthDate) => {
-    const today = new Date();
-    const birthYear = birthDate.getFullYear();
-    const birthMonth = birthDate.getMonth();
-    const birthDay = birthDate.getDate();
-
-    let age = today.getFullYear() - birthYear;
-
-    // Перевіряємо, якщо цьогорічний день народження вже минув чи ще ні
-    const isBirthdayPassed = today.getMonth() > birthMonth || (today.getMonth() === birthMonth && today.getDate() >= birthDay);
-
-    // Якщо день народження ще не був цього року, зменшуємо вік на 1
-    if (!isBirthdayPassed) {
-      age -= 1;
-    }
-
-    return age;
-  };
-
   return (
     <div className="greeting">
       {`My name is ${userData.firstName} ${userData.lastName}. 
