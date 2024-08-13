ea4870d6558b5177cfecf3dfa77b4d966fcae104
E-Martynenko1982 <motor8103@gmail.com>
2024-08-13 18:31:03 +0300

    update

---
+++ lesson-5-events/task1/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GoodButton from "./GoodButton";
+
+
+const App = () => {
+  return (
+    <GoodButton />
+  )
+};
+export default App;
---
+++ lesson-5-events/task1/src/GoodButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GoodButton = () => {
+  return (
+    <button class="fancy-button"
+      onClick={() => alert('Good job!')}
+    >
+      Click me!
+    </button>
+  )
+};
+export default GoodButton;
