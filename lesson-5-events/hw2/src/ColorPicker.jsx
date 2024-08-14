d87978cfe387496463955b425fc43648054526aa
E-Martynenko1982 <motor8103@gmail.com>
2024-08-14 20:07:51 +0300

    update

---
+++ lesson-5-events/hw2/src/ColorPicker.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    }
+  }
+  handleMouseCoral = () => {
+    this.setState({
+      title: 'Coral'
+    })
+  }
+
+  handleMouseAqua = () => {
+    this.setState({
+      title: 'Aqua'
+    })
+  }
+
+  handleMouseBisque = () => {
+    this.setState({
+      title: 'Bisque'
+    })
+  }
+
+  handleMouseLeave = () => {
+    this.setState({
+      title: ''
+    })
+  }
+  render() {
+    return (
+
+      <div>
+        <div class="picker__title">{this.state.title}</div>
+        <div>
+          <button class="picker__button picker__button_coral"
+            onMouseEnter={this.handleMouseCoral}
+            onMouseLeave={this.handleMouseLeave}
+          ></button>
+          <button class="picker__button picker__button_aqua"
+            onMouseEnter={this.handleMouseAqua}
+            onMouseLeave={this.handleMouseLeave}
+          ></button>
+          <button class="picker__button picker__button_bisque"
+            onMouseEnter={this.handleMouseBisque}
+            onMouseLeave={this.handleMouseLeave}
+          ></button>
+        </div>
+      </div>
+
+    )
+  }
+}
+export default ColorPicker;
