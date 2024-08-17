0bb7e0cb72ade06493a8b10a5460862948a4410b
E-Martynenko1982 <motor8103@gmail.com>
2024-08-17 12:31:40 +0300

    update

--- lesson-7-lists/hw1/src/TransactionsList.jsx
+++ lesson-7-lists/hw1/src/TransactionsList.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import Transaction from './Transaction';
 
-class TransactionsList extends React.Component {
-  constructor(props) {
-    super(props);
+const TransactionsList = (props) => {
+  return (
+    <ul className="transactions">
+      {props.transactions.map((transaction) => (
+        <Transaction key={transaction.id} {...transaction} />
+      ))}
+    </ul>
+  )
+};
 
-
-  }
-
-  render() {
-
-    return (
-      <>
-        <ul className="transactions">
-          {this.props.transactions.map((transaction) => (
-            <Transaction key={transaction.id} {...transaction} />
-          ))}
-        </ul>
-      </>
-    )
-  }
-}
 export default TransactionsList;
