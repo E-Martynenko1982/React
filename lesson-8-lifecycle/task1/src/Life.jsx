dae446ccbb9e495eb1ba3c2e18ea919ce1832e2c
E-Martynenko1982 <motor8103@gmail.com>
2024-08-20 13:16:23 +0300

    update

--- lesson-8-lifecycle/task1/src/Life.jsx
+++ lesson-8-lifecycle/task1/src/Life.jsx
@@ -3,36 +3,45 @@ import React from "react";
 class Life extends React.Component {
   constructor(props) {
     super(props);
-    console.log('constructor: good place to create state')
+    console.log('constructor: good place to create state');
 
+    this.state = {
+      count: 0, // Начальное состояние
+    };
   }
 
   componentDidMount() {
-    console.log('componentDidMount: API calls, subscriptions')
+    console.log('componentDidMount: API calls, subscriptions');
 
+    // Устанавливаем таймер, который изменяет состояние через 2 секунды
+    this.timer = setTimeout(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 2000);
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render')
-
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true; // Возвращаем true, чтобы позволить рендеринг
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
-
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed')
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
 
+    // Очищаем таймер при размонтировании компонента
+    clearTimeout(this.timer);
   }
 
   render() {
-    console.log('return React element to build DOM')
+    console.log('return React element to build DOM');
 
     return (
-      <div>Hello world</div>
-    )
+      <div>Hello world. Count: {this.state.count}</div>
+    );
   }
 }
+
 export default Life;
