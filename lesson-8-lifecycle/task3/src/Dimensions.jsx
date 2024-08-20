1ede46c646787356540b1cd45d7a12b43eb148ee
E-Martynenko1982 <motor8103@gmail.com>
2024-08-20 18:15:48 +0300

    update

---
+++ lesson-8-lifecycle/task3/src/Dimensions.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Dimensions extends React.Component {
+  constructor(props) {
+    super(props);
+    const { innerWidth, innerHeight } = window
+    this.state = {
+      width: innerWidth,
+      height: innerHeight,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  handleResize = (e) => {
+    const { innerWidth, innerHeight } = e.target
+    this.setState({
+      width: innerWidth,
+      height: innerHeight,
+    })
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
+  }
+  render() {
+    return (
+      <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>
+
+    )
+  }
+}
+export default Dimensions;
