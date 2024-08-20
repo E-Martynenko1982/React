b4edc3a8d243aa12f36078100cfef08a26fe7f06
E-Martynenko1982 <motor8103@gmail.com>
2024-08-20 13:12:42 +0300

    update

---
+++ lesson-8-lifecycle/task1/src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Life from "./Life";
+import './index.scss'
+
+
+
+
+const rootElement = document.querySelector('#root')
+
+
+
+ReactDOM.render(<Life />, rootElement)
