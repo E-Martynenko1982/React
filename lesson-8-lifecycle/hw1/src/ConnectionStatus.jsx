c941ad7a3d109996decaa240d81b0eb2605f53b5
E-Martynenko1982 <motor8103@gmail.com>
2024-08-20 21:27:40 +0300

    update

---
+++ lesson-8-lifecycle/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class ConnectionStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOnline: true
+    }
+  }
+
+  componentDidMount() {
+    //підписуємось на події зміни статусу зєднання
+    window.addEventListener('online', this.updateStatus);
+    window.addEventListener('offline', this.updateStatus);
+    // встановлюємо поточний стан зєднання
+    this.setState({ isOnline: navigator.onLine });
+  }
+  componentWillUnmount() {
+    //відписуємось на події зміни статусу зєднання
+    window.removeEventListener('online', this.updateStatus);
+    window.removeEventListener('offline', this.updateStatus);
+  }
+
+  updateStatus = () => {
+    this.setState({ isOnline: navigator.onLine })
+  }
+  render() {
+    const { isOnline } = this.state;
+    return (
+      <div className={`status ${isOnline ? '' : 'status_offline'}`}>
+        {isOnline ? 'Online' : 'Offline'}
+      </div>
+    )
+  }
+};
+export default ConnectionStatus;
