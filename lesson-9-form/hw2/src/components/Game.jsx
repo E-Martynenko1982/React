da2027fbf852450118625abc7a250758048e2e0f
E-Martynenko1982 <motor8103@gmail.com>
2024-08-21 17:20:51 +0300

    update

---
+++ lesson-9-form/hw2/src/components/Game.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Board from './Board';
+
+const Game = () => {
+  return (
+    <div className='wrapper'>
+      <Board />
+    </div>
+  )
+};
+
+export default Game
+
+
