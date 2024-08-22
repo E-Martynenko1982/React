0b28ce7899c59258adbf8d47b70f31e2295aa608
E-Martynenko1982 <motor8103@gmail.com>
2024-08-22 21:12:07 +0300

    update

---
+++ lesson-10-lifting-state-up/hw1/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Calculator from "./Calculator";
+import './index.scss'
+
+
+
+
+
+const rootElement = document.querySelector('#root')
+
+
+
+ReactDOM.render(<Calculator />, rootElement)
