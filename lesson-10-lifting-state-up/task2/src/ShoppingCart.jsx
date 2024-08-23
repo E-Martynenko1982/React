996eb85caff4262701d4fcb7848e898dbb52bb4d
E-Martynenko1982 <motor8103@gmail.com>
2024-08-23 13:23:52 +0300

    update

--- lesson-10-lifting-state-up/task2/src/ShoppingCart.jsx
+++ lesson-10-lifting-state-up/task2/src/ShoppingCart.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react';
-import CartTitle from './CartTitle';
-import ProductsList from './ProductsList';
+import React from "react";
+import CartTitle from "./CartTitle";
+import ProductsList from "./ProductsList";
 import './ShoppingCart.scss';
 
-const ShoppingCart = ({ userData }) => {
-  const [cartItems, setCartItems] = useState([
-    { id: '1', name: 'iPhone 11', price: 999 },
-    { id: '2', name: 'iPad Pro', price: 799 },
-  ]);
+class ShoppingCart extends React.Component {
+  state = {
+    cartItems: [
+      {
+        id: '1',
+        name: 'iPhone 11',
+        price: 999
+      },
+      {
+        id: '2',
+        name: 'iPad Pro',
+        price: 799
+      },
+    ]
+  };
 
-  const count = cartItems.length;
+  render() {
+    const count = this.state.cartItems.length;
+    return (
+      <div className="column">
 
-  return (
-    <div className="column">
-      <CartTitle userName={userData.firstName} count={count} />
-      <ProductsList cartItems={cartItems} />
-    </div>
-  );
-};
+        <CartTitle userName={this.props.userData.firstName} count={count} />
+        <ProductsList cartItems={this.state.cartItems} />
+      </div>
+    );
+  }
+}
 
 export default ShoppingCart;
