1057368d721bb6ff08bf159a1c0c38ee3dce720b
E-Martynenko1982 <motor8103@gmail.com>
2024-08-25 11:37:24 +0300

    update

--- lesson-11-component-update/hw1/src/Expand.jsx
+++ lesson-11-component-update/hw1/src/Expand.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Expand = ({ title, children, isOpen, onToggle }) => {
@@ -11,8 +12,23 @@ const Expand = ({ title, children, isOpen, onToggle }) => {
           <i className={`fas ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
         </button>
       </div>
-      {isOpen && <div className="expand__content">{children}</div>}
+      {isOpen && <div className="expand__content">
+
+        {children}</div>}
     </div>
   )
 };
+
+Expand.propTypes = {
+  isOpen: PropTypes.bool,
+  title: PropTypes.string,
+  children: PropTypes.element.isRequired,
+  onToggle: PropTypes.func.isRequired
+}
+Expand.defaultProps = {
+  isOpen: false,
+  title: "",
+}
+
+
 export default Expand;
