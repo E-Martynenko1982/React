d552ea4d4f19f5c1c123052b885bf661b9c59741
E-Martynenko1982 <motor8103@gmail.com>
2024-08-27 09:59:05 +0300

    update

--- lesson-12/todolist/src/Task.jsx
+++ lesson-12/todolist/src/Task.jsx
@@ -8,14 +8,15 @@ const Task = ({ id, done, text = '', onChange, onDelete }) => {
   return (
 
     < >
-      <li className={listItemClasses}>
-        <input type="checkbox"
-          className='list-item__checkbox'
-          defaultChecked={done}
+      <li className={`list-item${done ? ' list-item_done' : ''}`}>
+        <input
+          type="checkbox"
+          className="list-item__checkbox"
+          checked={done}
           onChange={() => onChange(id)}
         />
-        <span className='list-item__text'> {text}</span>
-        <button className='list-item__delete-btn' onClick={() => onDelete(id)}></button>
+        {text}
+        <button className="list-item__delete-btn" onClick={() => onDelete(id)} />
       </li>
     </>
   );
