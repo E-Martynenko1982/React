d88790172620ee5b5efbc9e6f806e16402e236fb
E-Martynenko1982 <motor8103@gmail.com>
2024-08-30 10:07:51 +0300

    update

--- lesson-14-hooks/task1/src/Counter.jsx
+++ lesson-14-hooks/task1/src/Counter.jsx
@@ -1,46 +1,15 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import './counter.scss';
 
 
 const Counter = () => {
-  const [counter, setCounter] = React.useState(0)
-
-  const deCrement = () => {
-    setCounter(counter - 1)
-  }
-
-  const inCrement = () => {
-    setCounter(counter + 1)
-  }
+  const [counter, setCounter] = useState(0)
   return (
     <div className="counter">
-      <button className="counter__button" onClick={deCrement}>-</button>
+      <button className="counter__button" onClick={() => setCounter(counter - 1)}>-</button>
       <span className="counter__value">{counter}</span>
-      <button className="counter__button" onClick={inCrement}>+</button>
+      <button className="counter__button" onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   )
 };
-
-// className Counter extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       counter: props.start
-//     }
-
-//     setInterval(() => {
-//       this.setState({
-//         counter: this.state.counter + 1
-//       });
-
-//     }, props.interval);
-//   }
-//   render() {
-//     return (
-//       <div classNameName="counter">
-//         {this.state.counter}
-//       </div>
-//     )
-//   }
-// }
 export default Counter;
